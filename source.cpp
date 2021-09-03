f3690a7d43c6acdc95dd10dd1228f0a35c8408e5
tukez <tuomo_fin@hotmail.com>
2021-09-03 11:44:27 +0300

    add new program for testing

--- source.cpp
+++ source.cpp
@@ -1,7 +1,17 @@
+#include <linux/i2c-dev.h>
+#include <i2c/smbus.h>
 #include <iostream>
+#include <stdint.h>
 
 int main(){
-    int num = 40;
-    std::cout << "hello from github" << std::endl;
-    std::cout << "this value is " << num << std::endl;
+    uint8_t devAddr = 0x1E;
+    uint8_t devReg = 0xD;
+    uint8_t retValue = 0;
+    char* i2cInterface = "/dev/i2c-3";
+
+    auto file = open(i2cInterface, O_RDWR);
+    ioctl(file, I2C_SLAVE, devAddr);
+    s_i2c_read_regs(file, devAddr, devReg, &retValue);
+    std::cout << "read from i2c " << retValue << std::endl;
+
 }
