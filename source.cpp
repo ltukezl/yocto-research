99d07db4519a11562d6569c52eefe7668743cc51
tukez <tuomo_fin@hotmail.com>
2021-09-17 11:29:50 +0300

    begin rdepends example

--- source.cpp
+++ source.cpp
@@ -1,25 +1,12 @@
-extern "C"{
-    #include <linux/i2c-dev.h>
-    #include <i2c/smbus.h>
-}
-#include <sys/ioctl.h>
-
-#include <stdio.h>
 #include <iostream>
-#include <stdint.h>
 #include <fstream>
-#include <string>
-#include <fcntl.h>
 
-int main(){
-    uint8_t devAddr = 0x1E;
-    uint8_t devReg = 0xD;
-    uint32_t retValue = 0;
-    std::string i2cInterface = "/dev/i2c-3";
+int main()
+{
+    std::ifstream f("file.txt");
 
-    auto file = open(i2cInterface.c_str(), I2C_RDWR);
-    ioctl(file, I2C_SLAVE, devAddr);
-    retValue = i2c_smbus_read_word_data(file, devReg);
-    std::cout << "read from i2c " << retValue << std::endl;
-
+    if (f.is_open())
+        std::cout << f.rdbuf();
+    else
+        std::cout << "File not in buildsystem" << std::endl;
 }
